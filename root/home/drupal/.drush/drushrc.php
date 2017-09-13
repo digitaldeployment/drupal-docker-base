263ac44a1858492026624d25953da9f7105b5221
Dennis Stevense <decafdennis@gmail.com>
2017-09-13 15:14:42 -0700

    Add default db-url for CI

--- root/home/drupal/.drush/drushrc.php
+++ root/home/drupal/.drush/drushrc.php
@@ -1,3 +1,5 @@
 <?php
 $options['r'] = '/var/drupal/www';
-$command_specific['site-install']['db-url'] = 'mysql://root@db/drupal';
+$command_specific['site-install']['db-url'] = empty($_ENV['CIRCLECI'])
+  ? 'mysql://root@db/drupal'
+  : 'mysql://root@127.0.0.1/circle_test';
